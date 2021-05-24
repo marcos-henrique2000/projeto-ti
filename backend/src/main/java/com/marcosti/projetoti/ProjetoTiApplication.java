16717ee1870b3e0638fc0682f407de685b6a0006
Marcos Marins <marcos-henrique2000@hotmail.com>
2021-05-24 10:44:51 -0300

    Project created

---
+++ backend/src/main/java/com/marcosti/projetoti/ProjetoTiApplication.java
@@ -0,0 +1,13 @@
+package com.marcosti.projetoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/marcosti/projetoti/ProjetoTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosti.projetoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
